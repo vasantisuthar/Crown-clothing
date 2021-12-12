2db0b874a570c9f2fa995eeec11903982cc041d0
Vasanti Suthar <vasantisuthar15@gmail.com>
2021-12-12 17:08:49 +0530

    created and styles homepage menu item

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './menu-item.styles.css';
+
+const MenuItem = ({title, imageUrl,size}) => {
+    return ( 
+        <div className={`${size} menu-item`}>
+            <div className="background-image" style={{backgroundImage:`url(${imageUrl})`}} />
+
+            <div className="content">
+                <h1 className="title">{title}</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+    </div>
+);
+}
+ 
+export default MenuItem;
