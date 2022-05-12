d2499040f1226669201279289a509369892944e0
Vasanti Suthar <vasantisuthar15@gmail.com>
2022-05-13 03:06:35 +0530

    added basename to browserouter

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import {store, persistor} from './redux/store';
 
 ReactDOM.render(
   <Provider store = {store}>
-    <BrowserRouter basename='/'>
+    <BrowserRouter basename='/index.html'>
     <PersistGate persistor={persistor}>
       <App />
     </PersistGate>
