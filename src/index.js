f27e5026e72f8b1073ecd8cb35841642df7313fd
Vasanti Suthar <vasantisuthar15@gmail.com>
2022-05-13 20:29:47 +0530

    changed basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import {store, persistor} from './redux/store';
 
 ReactDOM.render(
   <Provider store = {store}>
-    <BrowserRouter basename='/'>
+    <BrowserRouter basename='/app'>
     <PersistGate persistor={persistor}>
       <App />
     </PersistGate>
